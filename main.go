5ec0cb5a425e776f5e20de1baabd4fc303a1a76a
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-09-19 20:38:55 -0600

    add servemux, handler and server

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function
+func home(w http.ResponseWriter, r *http.Request) {
+	// Write a byte slice  as the response body
+	w.Write([]byte("Hello from snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world")
+	// Initialize a new servemux or router
+	mux := http.NewServeMux()
+
+	// register the home function as the handler for the "/" URL pattern
+	// "/" is a catch-all regardless of their URL path
+	mux.HandleFunc("/", home)
+
+	log.Print("starting server on :4000")
+
+	// start a new web server with ListenAndServe
+	err := http.ListenAndServe(":4000", mux)
+	// it returns an error that is always non-nil
+	log.Fatal(err)
 }
