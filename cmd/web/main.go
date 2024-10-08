64211040ff06ddb50bbe5e29938263d2249ff229
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-10-07 20:29:31 -0600

    move routes to a new file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,21 +20,9 @@ func main() {
 		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
 
-	// Initialize a new servemux or router
-	mux := http.NewServeMux()
-
-	fileHandler := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileHandler))
-
-	// register the home function as the handler for the "/" URL pattern
-	// "/" is a catch-all regardless of their URL path
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	app.logger.Info("starting server", slog.String("addr", *addr))
+
+	mux := app.routes()
 
 	// start a new web server with ListenAndServe
 	err := http.ListenAndServe(*addr, mux)
